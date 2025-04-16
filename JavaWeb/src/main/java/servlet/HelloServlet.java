5c821812a73a9203e504c6975881e2c2bab73ae3
NTPU <NTPU@503-tr>
2025-04-16 16:03:36 +0800

    web

---
+++ JavaWeb/src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// 表示瀏覽器可以透過 http://localhost:8080/JavaWeb/hello 網址執行到此 Servlet 程式
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().print("Hello JavaServlet"); // 將字串回應給瀏覽器
+	}
+	
+	
+}
