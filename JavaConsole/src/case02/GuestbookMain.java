36c041eaa730eedc39d6af269619ab00e46030fa
NTPU <NTPU@503-tr>
2025-04-21 15:50:02 +0800

    j

--- JavaConsole/src/case02/GuestbookMain.java
+++ JavaConsole/src/case02/GuestbookMain.java
@@ -9,7 +9,13 @@ public class GuestbookMain {
 	
 	public static void main(String[] args) {
 		// 留言
-		Guestbook g1 = new Guestbook("")
+		Guestbook g1 = new Guestbook("Hello 大家好");
+		guestbooks.add(g1); // 存入留言
+		
+		Guestbook g2 = new Guestbook("好像快要下雨了...");
+		guestbooks.add(g2); // 存入留言
+		
+		System.out.println(guestbooks);
 
 	}
 
