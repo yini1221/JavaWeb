f751273dc2ee181dd3a8330f225daedacce057ea
NTPU <NTPU@503-tr>
2025-04-22 11:57:03 +0800

    web

--- JavaConsole/src/case3/IceOrder.java
+++ JavaConsole/src/case3/IceOrder.java
@@ -5,4 +5,27 @@ public class IceOrder {
 	private Topping topping; // 配料
 	private int totalPrice; // 總價
 	
+	public IceOrder(String mainDishName, String[] toppingArray) {
+		this(new MainDish(mainDishName), new Topping(toppingArray));
+	}
+	
+	public IceOrder(MainDish mainDish, Topping topping) {
+		this.mainDish = mainDish;
+		this.topping = topping;
+		this.totalPrice = mainDish.getPrice() + topping.calculateToppingPrice();
+	}
+
+	public MainDish getMainDish() {
+		return mainDish;
+	}
+
+	public Topping getTopping() {
+		return topping;
+	}
+
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+	
+	
 }
