00b74c0a42cc699e38be3a9a7db23300515a3d37
NTPU <NTPU@503-tr>
2025-04-22 11:52:51 +0800

    web

--- JavaConsole/src/case3/Topping.java
+++ JavaConsole/src/case3/Topping.java
@@ -1,5 +1,6 @@
 package case3;
 
+import java.util.Arrays;
 import java.util.List;
 
 // 配料
@@ -7,6 +8,17 @@ public class Topping {
 	private List<String> toppings; // 配料集合
 	private static final int TOPPING_PRICE = 10;
 	
+	public Topping(String[] toppingArray) {
+		this.toppings = (toppingArray == null) ? null : Arrays.asList(toppingArray);
+	}
+
+	public List<String> getToppings() {
+		return toppings;
+	}
 	
+	// 計算配料總金額
+	public int calculateToppingPrice() {
+		return (toppings == null) ? 0 : toppings.size() * TOPPING_PRICE;
+	}
 	
 }
