f4791d7e0a4ff3621e4cd17ec654dc87a5b6c91b
NTPU <NTPU@503-tr>
2025-04-22 11:49:30 +0800

    web

--- JavaConsole/src/case3/Topping.java
+++ JavaConsole/src/case3/Topping.java
@@ -1,5 +1,12 @@
 package case3;
 
-public class Topping {
+import java.util.List;
 
+// 配料
+public class Topping {
+	private List<String> toppings; // 配料集合
+	private static final int TOPPING_PRICE = 10;
+	
+	
+	
 }
