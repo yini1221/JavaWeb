d8180caec3e85dfa2944b3c1f2e36ea3663f976c
NTPU <NTPU@503-tr>
2025-04-23 11:56:13 +0800

    web

--- JavaWebOrder/src/main/java/dao/OrderDAO.java
+++ JavaWebOrder/src/main/java/dao/OrderDAO.java
@@ -1,5 +1,23 @@
 package dao;
 
-public class OrderDAO {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import model.entity.Order;
+
+public class OrderDAO {
+	
+	// 利用 List 來模擬資料庫/資料表 (稱為:InMemoryDatabase)
+	private static List<Order> orders = new CopyOnWriteArrayList<>();
+	
+	// 存入一筆資訊
+	public void save(Order order) {
+		orders.add(order);
+	}
+	
+	// 取得所有歷史資料
+	public List<Order> findAll() {
+		return orders;
+	}
+	
 }
