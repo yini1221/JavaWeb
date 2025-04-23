0cc908b631ef0a8acb5c1607141793693f1e94a2
NTPU <NTPU@503-tr>
2025-04-23 14:57:06 +0800

    web

--- JavaWebOrder/src/main/java/dao/OrderDAO.java
+++ JavaWebOrder/src/main/java/dao/OrderDAO.java
@@ -25,4 +25,13 @@ public class OrderDAO {
 		orders.remove(index);
 	}
 	
+	// 修改一筆資訊
+	public void update(int index, Order newOrder) {
+		orders.set(index, newOrder);
+	}
+	
+	// 取得單筆資料
+	public Order getOrder(int index) {
+		return orders.get(index);
+	}
 }
