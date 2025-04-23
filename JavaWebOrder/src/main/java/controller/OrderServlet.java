5722b8c5b165e546537bd28d8ae3fd986b64cf1e
NTPU <NTPU@503-tr>
2025-04-23 12:28:39 +0800

    web

--- JavaWebOrder/src/main/java/controller/OrderServlet.java
+++ JavaWebOrder/src/main/java/controller/OrderServlet.java
@@ -31,7 +31,15 @@ public class OrderServlet extends HttpServlet {
 	// 接收訂單的請求
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		req.setCharacterEncoding("UTF-8");
+		// 取得使用者在表單中所訂購的商品
+		String item = req.getParameter("item"); 
+		// 新增訂單並會得到反饋
+		OrderDTO orderDTO = orderService.addOrder(item);
+		// 重導到指定 jsp 並帶上歷史反饋資料(OrderDTO)
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/result.jsp");
+		req.setAttribute("orderDTO", orderDTO);
+		rd.forward(req, resp);
 	}
 	
 }
