675037fccb8356d76704fdd2fd6dc505f288ead9
NTPU <NTPU@503-tr>
2025-04-23 11:46:09 +0800

    web

--- JavaWebOrder/src/main/java/model/entity/Order.java
+++ JavaWebOrder/src/main/java/model/entity/Order.java
@@ -1,6 +1,12 @@
 package model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
 	private String item;
 	private Integer price;
