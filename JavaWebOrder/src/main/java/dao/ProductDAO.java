40db5af5e5e3a4c67f9df88bb69cec5fdd742040
NTPU <NTPU@503-tr>
2025-04-23 16:27:04 +0800

    web

--- JavaWebOrder/src/main/java/dao/ProductDAO.java
+++ JavaWebOrder/src/main/java/dao/ProductDAO.java
@@ -7,7 +7,9 @@ import model.entity.Product;
 public class ProductDAO {
 	
 	private static List<Product> products = List.of(
-			new Product("牛肉麵", 150), new Product("陽春麵", 60), new Product("番茄麵", 100));
+			new Product("牛肉麵", 150), new Product("陽春麵", 60), 
+			new Product("番茄麵", 100), new Product("麵疙瘩", 90),
+			new Product("搶鍋麵", 110));
 	
 	public List<Product> findAll() {
 		return products;
