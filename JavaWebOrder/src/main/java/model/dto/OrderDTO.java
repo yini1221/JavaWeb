d16fde1d156862238452dd3cc2fc63a38b468cff
NTPU <NTPU@503-tr>
2025-04-23 11:47:29 +0800

    web

--- JavaWebOrder/src/main/java/model/dto/OrderDTO.java
+++ JavaWebOrder/src/main/java/model/dto/OrderDTO.java
@@ -1,5 +1,13 @@
 package model.dto;
 
-public class OrderDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDTO {
+	private String message; // 回應訂單結果資訊
+	
 }
